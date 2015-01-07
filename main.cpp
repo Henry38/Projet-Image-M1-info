05047625c364f303531a3dc30fada9c66f68435b
maximilien38 <maximilien-faure@hotmail.fr>
2015-01-07 17:46:09 +0100

    rien

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
 
-    //myWindow fen("../Projet-Image-M1-info/ressources/box.png");
-    myWindow fen;
+    myWindow fen("../Projet-Image-M1-info/ressources/box.png");
+    //myWindow fen;
     fen.show();
 
     return app.exec();
